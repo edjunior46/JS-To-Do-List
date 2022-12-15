f72050267723181b108e066d28c54bb2fbd61a87
Ed <ed.esmeraldino7@gmail.com>
2022-12-15 08:51:44 -0300

    adicionando funcionalidades

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,38 @@
 const taskInput = document.querySelector("#inputTasks")
-const addTaskBtn = document.querySelector("#addTasks button")
-const todoTasks = document.querySelector("#todoTasks")
+const addTasksBtn = document.querySelector("#addTasks button")
+const tasksList = document.querySelector("#todoTasks")
+let tasksArr = []
 
-let taskArr = []
-
-addTaskBtn.addEventListener('click', () => {
-    taskArr.push(createLi(taskInput.value))
-    addClear(taskArr[taskArr.length - 1])
-    todoTasks.innerHTML += taskArr[taskArr.length - 1].innerHTML
+addTasksBtn.addEventListener("click", () => {
+    if(taskInput.value){
+        tasksArr.push({
+            task: taskInput.value,
+            createAt: Date.now(),
+            checked: false,
+            order: (tasksArr.length + 1)
+        })
+        tasksList.innerHTML += createTask(tasksArr[tasksArr.length - 1])
+        console.log(tasksArr)
+        taskInput.value = ""
+        taskInput.focus()
+    } else {
+        alert("Insira um titulo para sua tarefa")
+    }
 })
 
-function createLi(txt) {
-    let newLi = document.createElement('li')
-    newLi.innerHTML = `<input type=" checkbox " name='taskCheck' id="taskCheck"/><span>${txt}</span><button class="taskEdit"/><button class="taskClear"/>`
-    return newLi
+function createTask(e) {
+    const newTask = document.createElement("LI")
+    newTask.innerHTML = `<input type="checkbox">
+    <span id="taskTitle">${e.task}</span>
+    <span id="taskDate"></span>
+    <button class="taskEdit">Edit</button>
+    <button class="taskClear" onclick="taskDelFn(this)" value="${e.order}">Clear</button>`
+    return newTask.outerHTML
 }
 
-function addClear(e) {
-    console.log(e)
+function taskDelFn(i) {
+    console.log(i.value)
+    tasksArr.splice((i.value - 1), 1)
+    console.log(tasksArr)
+    i.parentElement.remove()
 }
